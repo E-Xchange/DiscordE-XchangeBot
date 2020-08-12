ebafa5b1eb2c38bc3ee8370981c5c40e9f31c22e
Piotr Czupyk <piotr.czupyk@gmail.com>
2020-08-12 19:59:50 +0200

    CurrencyInfo fix

--- core/cryptoinfo.py
+++ core/cryptoinfo.py
@@ -2,7 +2,7 @@ from requests import Request
 import json
 import urllib
 
-url = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,DASH,BTC&tsyms=BTC,USD,EUR&api_key=55599765b9bb94391e2788c60fe9663e0e0eb6cfbb8f0ebf0fe08d4ca'
+url = 'https://min-api.cryptocompare.com/data/pricemulti?fsyms=ETH,DASH,BTC&tsyms=BTC,USD,EUR&api_key=55599765b9bb94391e2788c60fe9663e0e0eb6cfbb8f0ebf0fe08d4ca802f4e0'
 open_url = urllib.request.urlopen(url)
 
 
--- core/currencyinfo.py
+++ core/currencyinfo.py
@@ -7,30 +7,24 @@ url_EUR = 'http://api.nbp.pl/api/exchangerates/rates/a/eur/'
 url_AUD = 'http://api.nbp.pl/api/exchangerates/rates/a/aud/'
 irl_CHF = 'http://api.nbp.pl/api/exchangerates/rates/a/chf/'
 
-
 open_url_USD = urllib.request.urlopen(url_USD)
 open_url_EUR = urllib.request.urlopen(url_EUR)
 open_url_AUD = urllib.request.urlopen(url_AUD)
 open_url_CHF = urllib.request.urlopen(irl_CHF)
 
-
 data_USD = open_url_USD.read()
 json_data_USD = json.loads(data_USD)
-
-data_EUR = open_url_EUR.read()
-json_data_EUR = json.loads(data_EUR)
-
-data_AUD = open_url_AUD.read()
-json_data_AUD = json.loads(data_AUD)
-
-data_CHF = open_url_CHF.read()
-json_data_CHF = json.loads(data_CHF)
-
 base_USD_price = json_data_USD['rates'][0]['mid']
-EUR_price = json_data_EUR['rates'][0]['mid']/base_USD_price
-AUD_price = json_data_AUD['rates'][0]['mid']/base_USD_price
-CHF_price = json_data_CHF['rates'][0]['mid']/base_USD_price
 
-print(round(EUR_price, 2))
-print(round(AUD_price, 2))
-print(round(CHF_price, 2))
+
+def data(url_open):
+    data = url_open.read()
+    json_data = json.loads(data)
+    price = json_data['rates'][0]['mid']/base_USD_price
+    print(round(price,2))
+
+
+data(open_url_EUR)
+data(open_url_AUD)
+data(open_url_CHF)
+
