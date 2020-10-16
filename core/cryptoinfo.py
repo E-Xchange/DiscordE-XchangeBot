0d41132e1b9d00378c141aced74ea4344211da14
Piotr Czupyk <szkycode@gmail.com>
2020-10-17 00:24:27 +0200

    test

--- core/cryptoinfo.py
+++ core/cryptoinfo.py
@@ -14,3 +14,5 @@ bitcoin_price = round(json_data['BTC']['USD'], 2)
 ethereum_price = round(json_data['ETH']['USD'], 2)
 cardano_price = round(json_data['ADA']['USD'], 2)
 addcrypto(bitcoin_price, 'BTC')
+
+#testrepo
