d77750709928201d7b7e8bb49697e32e4c16be84
Piotr Czupyk <szkycode@gmail.com>
2020-11-14 23:01:00 +0100

    list of comand work! 14.11

--- core/main.py
+++ core/main.py
@@ -3,6 +3,7 @@ import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 from config import help_hint as hint
+import asyncio
 
 load_dotenv()
 KEY_API = os.getenv('DISCORD_TOKEN')
@@ -11,7 +12,7 @@ intents = discord.Intents.default()
 intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
-
+bot.remove_command("help")
 
 @bot.event
 async def on_ready():
@@ -25,12 +26,23 @@ async def on_member_join(member):
         f'Hello {member.name}, now I will keep you informed about news from the financial world. Good luck'
     )
 
+@bot.command(name="help")
+async def helpComand(ctx):
+    await ctx.author.create_dm()
+    await ctx.author.dm_channel.send(f"```!price -> {hint.price_comand_help} \n```")
+
+
 
 @bot.command(name="price", help=hint.price_comand_help)
 async def price(ctx):
-    await ctx.send("Check price of metals: !metals \n"
-                   "Check price of crypto: !crypto \n"
-                   "Check price of currency: !currency \n")
+    await ctx.author.create_dm()
+    await ctx.author.dm_channel.send("``` Check price of currency -> !currency \n\n"
+                   " Check price of crypto -> !crypto \n\n"
+                   " Check price of metals -> !metals ```\n\n")
+
+
+
+
 
 
 
