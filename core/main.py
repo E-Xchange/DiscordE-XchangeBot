ef7851c5511fb91f3f230f223c30ae06c13f7529
Piotr Czupyk <szkycode@gmail.com>
2020-11-14 23:25:49 +0100

    Embed bug! 14.11

--- core/main.py
+++ core/main.py
@@ -3,7 +3,6 @@ import discord
 from dotenv import load_dotenv
 from discord.ext import commands
 from config import help_hint as hint
-import asyncio
 
 load_dotenv()
 KEY_API = os.getenv('DISCORD_TOKEN')
@@ -13,6 +12,7 @@ intents.members = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 bot.remove_command("help")
+
 
 @bot.event
 async def on_ready():
@@ -26,25 +26,20 @@ async def on_member_join(member):
         f'Hello {member.name}, now I will keep you informed about news from the financial world. Good luck'
     )
 
+
 @bot.command(name="help")
 async def helpComand(ctx):
+    embed = discord.Embed(title="Helper", description=f"!print -> {hint.price_comand_help}", color=0x00ff00)
+    embed.set_footer(text="E-Xchange")
     await ctx.author.create_dm()
-    await ctx.author.dm_channel.send(f"```!price -> {hint.price_comand_help} \n```")
-
-
+    await ctx.author.dm_channel.send(embed=embed)
 
 @bot.command(name="price", help=hint.price_comand_help)
 async def price(ctx):
     await ctx.author.create_dm()
     await ctx.author.dm_channel.send("``` Check price of currency -> !currency \n\n"
-                   " Check price of crypto -> !crypto \n\n"
-                   " Check price of metals -> !metals ```\n\n")
-
-
-
-
-
-
+                                     " Check price of crypto -> !crypto \n\n"
+                                     " Check price of metals -> !metals ```\n\n")
 
 
 bot.run(KEY_API)
